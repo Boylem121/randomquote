fc3078c860c5c890f7b3c0a549d12b1f72c133d5
Boylem121 <Boylem121@gmail.com>
2017-10-15 20:15:05 -0500

    Fixed emptey quote on first load

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
  $(document).ready(function() {
 
+   $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(value) {
+     tweet = 'https://twitter.com/intent/tweet?text=' + value.quote;
+     $('#quote-place').html('<h2 id="quote">' + value.quote + '</h2>');
+     preview[0].setAttribute('href', tweet);
+   });
+
    var tweet = "";
-   var preview = document.querySelectorAll ("#tweet-link");
+   var preview = document.querySelectorAll("#tweet-link");
 
    $("#new-quote").click(function() {
      $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(value) {
